cbc472ff6dd0cec50b86708f185d0c8a426b35bc
jwaldrip <jason@waldrip.net>
2016-07-28 17:24:41 -0600

    more fixes

--- promise.cr
+++ promise.cr
@@ -1,4 +1,5 @@
 class Promise(T)
+  class HaltException < Exception ; end
 
   def initialize(&block : (T -> Nil) -> _)
     initialize do |resolve, _|
@@ -6,39 +7,71 @@ class Promise(T)
     end
   end
 
-
-  def initialize(&block : (T -> Nil), (Exception -> Nil) -> _)
+  def initialize(&block : (T -> Nil), (String -> Nil) -> _)
     @resolution = Channel::Buffered(T).new(1)
     @rejection  = Channel::Buffered(Exception).new(1)
     @waiter     = Channel::Buffered(Bool).new(1)
     resolve = ->(value : T) {
+      @rejection.close
       @resolution.send(value)
       @waiter.send(true)
       nil
     }
     reject = ->(err : Exception) {
+      @resolution.close
       @rejection.send(err)
       @waiter.send(true)
       nil
     }
     spawn do
       begin
-        block.call(resolve, reject)
+        block.call(resolve, ->(message : String){ reject.call(Exception.new(message)) })
       rescue ex : Exception
         reject.call(ex)
       end
     end
   end
 
+  def then(&block)
+    Promise(T | Nil).new do |resolve|
+      @resolution.receive
+      resolve.call(block.call)
+    end
+  end
+
   def then(&block : T -> _)
-    Promise(T | Nil).new do |resolve, reject|
-      resolve.call(block.call(@resolution.receive))
+    Promise(T | Nil).new do |resolve|
+      begin
+        resolve.call(block.call(get_resolution))
+      rescue ex : HaltException
+        nil
+      end
     end
   end
 
   def catch(&block : Exception -> _)
-    Promise(T | Nil).new do |resolve, reject|
-      resolve.call(block.call(@rejection.receive))
+    Promise(T | Nil).new do |resolve|
+      begin
+        resolve.call(block.call(get_rejection))
+      rescue ex : HaltException
+        self.then { |result| resolve.call(result) }
+      end
+    end
+  end
+
+  private def get_resolution
+    begin
+      @resolution.receive
+    rescue ex : Channel::ClosedError
+      raise HaltException.new(message: ex.message, cause: ex)
+    end
+  end
+
+  private def get_rejection
+    begin
+      @rejection.receive
+    rescue ex : Channel::ClosedError
+      raise HaltException.new(message: ex.message, cause: ex)
     end
   end
 
@@ -47,22 +80,3 @@ class Promise(T)
   end
 
 end
-
-promises = [] of Promise(String | Nil)
-10.times do
-  promises << Promise(String).new do |resolve|
-    sleep 3
-    resolve.call("done")
-  end.then do |result|
-    "result: #{result}"
-  end.then do |result|
-    puts result
-    raise "Whoops"
-  end.catch do |ex|
-    ex.message
-  end.then do |message|
-    puts message
-  end
-end
-
-promises.each(&.wait)
